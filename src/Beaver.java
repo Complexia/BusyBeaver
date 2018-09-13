1e46ec97c1a8742c198bae6a3d2c2e9e21469045
s3399752 <s3399752@student.rmit.edu.au>
2018-09-14 01:51:46 +1000

    something absolutely astoundingly amazing

--- src/Beaver.java
+++ src/Beaver.java
@@ -1,15 +1,37 @@
+import java.util.ArrayList;
 
 public class Beaver {
 	
 	static String currentState = "a";
+	static ArrayList<String> patternA = new ArrayList();
+	static ArrayList<String> patternB = new ArrayList();
+	static ArrayList<String> patternC = new ArrayList();
+	static ArrayList<String> patternA1 = new ArrayList();
+	static ArrayList<String> patternB1 = new ArrayList();
+	static ArrayList<String> patternC1 = new ArrayList();
+	static ArrayList<String> patternD0 = new ArrayList();
+	static ArrayList<String> patternD1 = new ArrayList();
+	static ArrayList<String> patternE0 = new ArrayList();
+	static ArrayList<String> patternE1 = new ArrayList();
+	static ArrayList<String> currentPattern = new ArrayList();
+	static ArrayList<String> currentPatternA1 = new ArrayList();
+	static ArrayList<String> currentPatternB0 = new ArrayList();
+	static ArrayList<String> currentPatternB1 = new ArrayList();
+	static ArrayList<String> currentPatternC0 = new ArrayList();
+	static ArrayList<String> currentPatternC1 = new ArrayList();
+	static ArrayList<String> currentPatternD0 = new ArrayList();
+	static ArrayList<String> currentPatternD1 = new ArrayList();
+	static ArrayList<String> currentPatternE0 = new ArrayList();
+	static ArrayList<String> currentPatternE1 = new ArrayList();
 	static LinkedNode currentPosition = new LinkedNode(0,null,null);
 	public static void main(String[]args) {
+		
 		
 		int steps = 0;
 		int ones = 0;
 		while(!currentState.equalsIgnoreCase("h")) {
 			
-			instruction5State(currentPosition,currentState);
+			instruction3State(currentPosition,currentState);
 			steps++;
 		}
 		
@@ -37,6 +59,24 @@ public class Beaver {
 		if(state.equalsIgnoreCase("a")) {
 			if(position.getSymbol() == 0) {
 				
+				
+				
+				if(!currentPattern.isEmpty()) {					
+					if(patternA.equals(currentPattern)) {
+						System.out.println("looping");						
+					}					
+				}
+				patternA = currentPattern;
+				currentPattern = new ArrayList();				
+				currentPattern.add("a0");
+				currentPatternA1.add("a0");
+				currentPatternB0.add("a0");
+				currentPatternB1.add("a0");
+				
+				
+				
+				
+				
 				Beaver.currentState = "b";
 				Beaver.currentPosition.setSymbol(1);
 				if(position.getChildNode() == null) {
@@ -49,6 +89,19 @@ public class Beaver {
 				}
 			}
 			else if(position.getSymbol() == 1) {
+				
+				if(!currentPatternA1.isEmpty()) {					
+					if(patternA1.equals(currentPatternA1)) {
+						System.out.println("looping");						
+					}					
+				}
+				patternA1 = currentPattern;
+				currentPatternA1 = new ArrayList();				
+				currentPattern.add("a1");
+				currentPatternA1.add("a1");
+				currentPatternB0.add("a1");
+				currentPatternB1.add("a1");
+				
 				Beaver.currentState ="b";
 				Beaver.currentPosition.setSymbol(1);
 				if(position.getParentNode() == null) {
@@ -63,6 +116,19 @@ public class Beaver {
 		else if(state.equalsIgnoreCase("b")) {
 			
 			if(position.getSymbol() == 0) {
+				
+				if(!currentPatternB0.isEmpty()) {					
+					if(patternB.equals(currentPatternB0)) {
+						System.out.println("looping");						
+					}					
+				}
+				patternB = currentPatternB0;
+				currentPatternB0 = new ArrayList();				
+				currentPattern.add("b0");
+				currentPatternA1.add("b0");
+				currentPatternB0.add("b0");
+				currentPatternB1.add("b0");
+				
 				Beaver.currentState = "a";
 				Beaver.currentPosition.setSymbol(1);
 				if(position.getParentNode() == null) {
@@ -73,15 +139,40 @@ public class Beaver {
 					Beaver.currentPosition = Beaver.currentPosition.getParentNode();
 				}
 			}
-			else if(position.getSymbol() == 1) {
-				Beaver.currentState ="h";
+			else if(position.getSymbol() == 1) {			
+				String direction = "l";
+				
+				if(!currentPatternB1.isEmpty()) {					
+					if(patternB1.equals(currentPatternB1)) {
+						System.out.println("looping");						
+					}					
+				}
+				patternB1 = currentPatternB1;
+				currentPatternB1 = new ArrayList();				
+				currentPattern.add("b1");
+				currentPatternA1.add("b1");
+				currentPatternB0.add("b1");
+				currentPatternB1.add("b1");
+				
+				Beaver.currentState ="h"; //halt
 				Beaver.currentPosition.setSymbol(1);
-				if(position.getChildNode() == null) {
-					Beaver.currentPosition.setParentNode(new LinkedNode(0,Beaver.currentPosition, null));
-					Beaver.currentPosition = Beaver.currentPosition.getChildNode();
+				if(direction.equalsIgnoreCase("r")) {
+					if(position.getChildNode() == null) {				
+						Beaver.currentPosition.setChildNode(new LinkedNode(0,Beaver.currentPosition,null));
+						Beaver.currentPosition = Beaver.currentPosition.getChildNode();
+					}
+					else {
+						Beaver.currentPosition = Beaver.currentPosition.getChildNode();
+					}
 				}
 				else {
-					Beaver.currentPosition = Beaver.currentPosition.getChildNode();
+					if(position.getParentNode() == null) {
+						Beaver.currentPosition.setParentNode(new LinkedNode(0, null,Beaver.currentPosition));
+						Beaver.currentPosition = Beaver.currentPosition.getParentNode();
+					}
+					else {
+						Beaver.currentPosition = Beaver.currentPosition.getParentNode();
+					}
 				}
 			}
 			
@@ -93,6 +184,33 @@ public static void instruction3State(LinkedNode position, String state) {
 		
 		if(state.equalsIgnoreCase("a")) {
 			if(position.getSymbol() == 0) {
+				
+				
+				if(!currentPattern.isEmpty()) {					
+					if(patternA.equals(currentPattern)) {
+						System.out.println("looping");						
+					}					
+				}
+				patternA = currentPattern;
+				currentPattern = new ArrayList();				
+				currentPattern.add("a0");
+				if(!currentPatternA1.isEmpty()) {
+					currentPatternA1.add("a0");
+				}
+				
+				if(!currentPatternB0.isEmpty()) {
+					currentPatternB0.add("a0");
+				}
+				if(!currentPatternB1.isEmpty()) {
+					currentPatternB1.add("a0");
+				}
+				if(!currentPatternC0.isEmpty()) {
+					currentPatternC0.add("a0");
+				}
+				if(!currentPatternC1.isEmpty()) {
+					currentPatternC1.add("a0");
+				}
+				
 				
 				Beaver.currentState = "b";
 				Beaver.currentPosition.setSymbol(1);
@@ -106,7 +224,35 @@ public static void instruction3State(LinkedNode position, String state) {
 				}
 			}
 			else if(position.getSymbol() == 1) {
-				Beaver.currentState ="h";
+				
+				if(!currentPatternA1.isEmpty()) {					
+					if(patternA1.equals(currentPatternA1) && patternA1.size() >1) {
+						System.out.println("looping");						
+					}					
+				}
+				patternA1 = currentPatternA1;
+				currentPatternA1 = new ArrayList();
+				currentPatternA1.add("a1");
+				
+				if(!currentPattern.isEmpty()) {
+					currentPattern.add("a1");
+				}
+				
+				if(!currentPatternB0.isEmpty()) {
+					currentPatternB0.add("a1");
+				}
+				if(!currentPatternB1.isEmpty()) {
+					currentPatternB1.add("a1");
+				}
+				if(!currentPatternC0.isEmpty()) {
+					currentPatternC0.add("a1");
+				}
+				if(!currentPatternC1.isEmpty()) {
+					currentPatternC1.add("a1");
+				}
+				
+				
+				Beaver.currentState ="h"; //halt
 				Beaver.currentPosition.setSymbol(1);
 				if(position.getChildNode() == null) {
 					Beaver.currentPosition.setChildNode(new LinkedNode(0,Beaver.currentPosition,null));
@@ -120,6 +266,35 @@ public static void instruction3State(LinkedNode position, String state) {
 		else if(state.equalsIgnoreCase("b")) {
 			
 			if(position.getSymbol() == 0) {
+				
+				
+				if(!currentPatternB0.isEmpty()) {					
+					if(patternB.equals(currentPatternB0)) {
+						System.out.println("looping");						
+					}					
+				}
+				patternB = currentPatternB0;
+				currentPatternB0 = new ArrayList();	
+				currentPatternB0.add("b0");
+				
+				if(!currentPattern.isEmpty()) {
+					currentPattern.add("b0");
+				}
+				
+				if(!currentPatternA1.isEmpty()) {
+					currentPatternA1.add("b0");
+				}
+				if(!currentPatternC0.isEmpty()) {
+					currentPatternC0.add("b0");
+				}
+				if(!currentPatternC1.isEmpty()) {
+					currentPatternC1.add("b0");
+				}
+				if(!currentPatternC1.isEmpty()) {
+					currentPatternC1.add("a0");
+				}
+				
+				
 				Beaver.currentState = "c";
 				Beaver.currentPosition.setSymbol(0);
 				if(position.getChildNode() == null) {
@@ -131,6 +306,33 @@ public static void instruction3State(LinkedNode position, String state) {
 				}
 			}
 			else if(position.getSymbol() == 1) {
+				
+				if(!currentPatternB1.isEmpty()) {					
+					if(patternB1.equals(currentPatternB1) && patternB1.size() > 1) {
+						System.out.println("looping");						
+					}					
+				}
+				patternB1 = currentPatternB1;
+				currentPatternB1 = new ArrayList();	
+				currentPatternB1.add("b1");
+				
+				if(!currentPattern.isEmpty()) {
+					currentPattern.add("b1");
+				}
+				
+				if(!currentPatternA1.isEmpty()) {
+					currentPatternA1.add("b1");
+				}
+				if(!currentPatternB0.isEmpty()) {
+					currentPatternB0.add("b1");
+				}
+				if(!currentPatternC0.isEmpty()) {
+					currentPatternC0.add("b1");
+				}
+				if(!currentPatternC1.isEmpty()) {
+					currentPatternC1.add("b1");
+				}
+				
 				Beaver.currentState ="b";
 				Beaver.currentPosition.setSymbol(1);
 				if(position.getChildNode() == null) {
@@ -146,6 +348,33 @@ public static void instruction3State(LinkedNode position, String state) {
 		else if(state.equalsIgnoreCase("c")) {
 			
 			if(position.getSymbol() == 0) {
+				
+				if(!currentPatternC0.isEmpty()) {					
+					if(patternC.equals(currentPatternC0)) {
+						System.out.println("looping");						
+					}					
+				}
+				patternC = currentPatternC0;
+				currentPatternC0 = new ArrayList();				
+				currentPatternC0.add("c0");
+				
+				if(!currentPattern.isEmpty()) {
+					currentPattern.add("c0");
+				}
+				
+				if(!currentPatternA1.isEmpty()) {
+					currentPatternA1.add("c0");
+				}
+				if(!currentPatternB0.isEmpty()) {
+					currentPatternB0.add("c0");
+				}
+				if(!currentPatternB1.isEmpty()) {
+					currentPatternB1.add("c0");
+				}
+				if(!currentPatternC1.isEmpty()) {
+					currentPatternC1.add("c0");
+				}
+				
 				Beaver.currentState = "c";
 				Beaver.currentPosition.setSymbol(1);
 				if(position.getParentNode() == null) {
@@ -157,6 +386,33 @@ public static void instruction3State(LinkedNode position, String state) {
 				}
 			}
 			else if(position.getSymbol() == 1) {
+				
+				if(!currentPatternC1.isEmpty()) {					
+					if(patternC1.equals(currentPatternC1) && patternC1.size() > 1) {
+						System.out.println("looping");						
+					}					
+				}
+				patternC1 = currentPatternC1;
+				currentPatternC1 = new ArrayList();
+				currentPatternC1.add("c1");
+				
+				if(!currentPattern.isEmpty()) {
+					currentPattern.add("c1");
+				}
+				
+				if(!currentPatternA1.isEmpty()) {
+					currentPatternA1.add("c1");
+				}
+				if(!currentPatternB0.isEmpty()) {
+					currentPatternB0.add("c1");
+				}
+				if(!currentPatternB1.isEmpty()) {
+					currentPatternB1.add("c1");
+				}
+				if(!currentPatternC0.isEmpty()) {
+					currentPatternC0.add("c1");
+				}
+				
 				Beaver.currentState ="a";
 				Beaver.currentPosition.setSymbol(1);
 				if(position.getParentNode() == null) {
@@ -383,6 +639,34 @@ public static void instruction5State(LinkedNode position, String state) {
 	
 	if(state.equalsIgnoreCase("a")) {
 		if(position.getSymbol() == 0) {
+			
+			
+			if(!currentPattern.isEmpty()) {					
+				if(patternA.equals(currentPattern)) {
+					System.out.println("looping");						
+				}					
+			}
+			patternA = currentPattern;
+			currentPattern = new ArrayList();				
+			currentPattern.add("a0");
+			if(!currentPatternA1.isEmpty()) {
+				currentPatternA1.add("a0");
+			}
+			
+			if(!currentPatternB0.isEmpty()) {
+				currentPatternB0.add("a0");
+			}
+			if(!currentPatternB1.isEmpty()) {
+				currentPatternB1.add("a0");
+			}
+			if(!currentPatternC0.isEmpty()) {
+				currentPatternC0.add("a0");
+			}
+			if(!currentPatternC1.isEmpty()) {
+				currentPatternC1.add("a0");
+			}
+			
+			
 			String direction = "l";
 			Beaver.currentState = "b";
 			Beaver.currentPosition.setSymbol(1);
@@ -407,6 +691,7 @@ public static void instruction5State(LinkedNode position, String state) {
 			
 		}
 		else if(position.getSymbol() == 1) {
+			currentPattern.add("a1");
 			String direction = "l";
 			Beaver.currentState ="a";
 			Beaver.currentPosition.setSymbol(1);
@@ -433,6 +718,7 @@ public static void instruction5State(LinkedNode position, String state) {
 	else if(state.equalsIgnoreCase("b")) {
 		
 		if(position.getSymbol() == 0) {
+			currentPattern.add("b0");
 			String direction = "r";
 			Beaver.currentState = "c";
 			Beaver.currentPosition.setSymbol(1);
@@ -456,6 +742,7 @@ public static void instruction5State(LinkedNode position, String state) {
 			}
 		}
 		else if(position.getSymbol() == 1) {
+			currentPattern.add("b1");
 			String direction = "r";
 			Beaver.currentState ="b";
 			Beaver.currentPosition.setSymbol(1);
@@ -483,6 +770,7 @@ public static void instruction5State(LinkedNode position, String state) {
 	else if(state.equalsIgnoreCase("c")) {
 		
 		if(position.getSymbol() == 0) {
+			currentPattern.add("c0");
 			String direction = "l";
 			Beaver.currentState = "a";
 			Beaver.currentPosition.setSymbol(1);
@@ -506,6 +794,7 @@ public static void instruction5State(LinkedNode position, String state) {
 			}
 		}
 		else if(position.getSymbol() == 1) {
+			currentPattern.add("c1");
 			String direction = "r";
 			Beaver.currentState ="d";
 			Beaver.currentPosition.setSymbol(1);
@@ -534,6 +823,7 @@ public static void instruction5State(LinkedNode position, String state) {
 	else if(state.equalsIgnoreCase("d")) {
 		
 		if(position.getSymbol() == 0) {
+			currentPattern.add("d0");
 			String direction = "l";
 			Beaver.currentState = "a";
 			Beaver.currentPosition.setSymbol(1);
@@ -557,6 +847,7 @@ public static void instruction5State(LinkedNode position, String state) {
 			}
 		}
 		else if(position.getSymbol() == 1) {
+			currentPattern.add("d1");
 			String direction = "r";
 			Beaver.currentState ="e";
 			Beaver.currentPosition.setSymbol(1);
@@ -583,8 +874,9 @@ public static void instruction5State(LinkedNode position, String state) {
 	}
 	if(state.equalsIgnoreCase("e")) {
 		if(position.getSymbol() == 0) {
+			currentPattern.add("e0");
 			String direction = "r";
-			Beaver.currentState = "h";
+			Beaver.currentState = "h"; //halt
 			Beaver.currentPosition.setSymbol(1);
 			if(direction.equalsIgnoreCase("r")) {
 				if(position.getChildNode() == null) {				
@@ -607,6 +899,7 @@ public static void instruction5State(LinkedNode position, String state) {
 			
 		}
 		else if(position.getSymbol() == 1) {
+			currentPattern.add("e1");
 			String direction = "r";
 			Beaver.currentState ="c";
 			Beaver.currentPosition.setSymbol(0);
